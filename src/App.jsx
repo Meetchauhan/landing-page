3c3e303c382a135de4cb0b91ea1af7f661cf5243
Meet Chauhan <meetchauhan1999@gmail.com>
2025-07-17 09:25:08 +0530

    about section removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,56 @@
-import './App.css';
-import AboutUs from './component/about/AboutUs';
-import Client from './component/client/Client';
-import Footer from './component/footer/Footer';
-import Header from './component/header/Header';
-import Hero from './component/hero/Hero';
-import IntroSection from './component/introSection/IntroSection';
-import OurMission from './component/mission/OurMission';
-import Services from './component/services/Services';
-import OurTeam from './component/team/OurTeam';
-
+import "./App.css";
+import AboutUs from "./component/about/AboutUs";
+import Client from "./component/client/Client";
+import Footer from "./component/footer/Footer";
+import Header from "./component/header/Header";
+import Hero from "./component/hero/Hero";
+import IntroSection from "./component/introSection/IntroSection";
+import OurMission from "./component/mission/OurMission";
+import Services from "./component/services/Services";
+import OurTeam from "./component/team/OurTeam";
 
 const services = [
   {
-    title: 'Supply Chain/Line',
-    desc: 'Specialises in Supply Chain/Line',
-    icon: '🚚',
+    title: "Supply Chain/Line",
+    desc: "Specialises in Supply Chain/Line",
+    icon: "🚚",
   },
   {
-    title: 'Live Tracking',
-    desc: 'Live tracking of all vehicles',
-    icon: '📡',
+    title: "Live Tracking",
+    desc: "Live tracking of all vehicles",
+    icon: "📡",
   },
   {
-    title: 'Secure Shipping',
-    desc: 'Secure shipping',
-    icon: '🔒',
+    title: "Secure Shipping",
+    desc: "Secure shipping",
+    icon: "🔒",
   },
   {
-    title: 'Capacity & Flexibility',
-    desc: 'Large Capacity and Flexibility',
-    icon: '📦',
+    title: "Capacity & Flexibility",
+    desc: "Large Capacity and Flexibility",
+    icon: "📦",
   },
   {
-    title: 'Timely Delivery',
-    desc: 'Timely Delivery',
-    icon: '⏱️',
+    title: "Timely Delivery",
+    desc: "Timely Delivery",
+    icon: "⏱️",
   },
   {
-    title: 'Warehouse Management',
-    desc: 'Warehouse Management',
-    icon: '🏢',
+    title: "Warehouse Management",
+    desc: "Warehouse Management",
+    icon: "🏢",
   },
   {
-    title: 'Competitive Prices',
-    desc: 'Competitive Prices',
-    icon: '💸',
+    title: "Competitive Prices",
+    desc: "Competitive Prices",
+    icon: "💸",
   },
   {
-    title: 'Future Oriented IT',
-    desc: 'Use of future oriented IT services',
-    icon: '💻',
+    title: "Future Oriented IT",
+    desc: "Use of future oriented IT services",
+    icon: "💻",
   },
 ];
-
-
 
 function App() {
   return (
@@ -65,7 +62,7 @@ function App() {
       <Services />
       <Client />
       <OurTeam />
-      <AboutUs />
+      {/* <AboutUs /> */}
       <Footer />
     </div>
   );
